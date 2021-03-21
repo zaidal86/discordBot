f0a89f498fa4736b7441a8437863048dfcc1af38
william <william-vivier@hotmail.fr>
2021-03-21 13:20:23 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const {discord} = require('./config/tokens.json');
+const {prefix} = require('./config/config.json');
+
+client.commands = new Discord.Collection();
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (message.content.indexOf(prefix) !== 0) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  const cmd = client.commands.get(command);
+  if (!cmd) return;
+
+  cmd.run(client, message, args);
+});
+
+fs.readdir('./events/', (err, files) => {
+    if (err) return console.error;
+    files.forEach(file => {
+      if (!file.endsWith('.js')) return;
+      const evt = require(`./events/${file}`);
+      let evtName = file.split('.')[0];
+      console.log(`Loaded event '${evtName}'`);
+      client.on(evtName, evt.bind(null, client));
+    });
+});
+
+fs.readdir('./commands/', async (err, files) => {
+    if (err) return console.error;
+    files.forEach(file => {
+      if (!file.endsWith('.js')) return;
+      let props = require(`./commands/${file}`);
+      let cmdName = file.split('.')[0];
+      console.log(`Loaded command '${cmdName}'`);
+      client.commands.set(cmdName, props);
+    });
+});
+
+client.login(discord);
